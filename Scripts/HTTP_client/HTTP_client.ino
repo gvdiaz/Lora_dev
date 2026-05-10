77abda1ed011210b824ba8c474feb070c6802861
Gustavo V. Diaz <gus838@gmail.com>
2026-05-10 18:50:33 -0300

    Primera versión funcionando la impresión en display OLED.

--- Scripts/HTTP_client/HTTP_client.ino
+++ Scripts/HTTP_client/HTTP_client.ino
@@ -13,11 +13,17 @@
 #include <Wire.h>
 #include "HT_SSD1306Wire.h"
 
+// Biblioteca para conversión de hora a formato humano
+#include <Arduino.h>
+#include <TimeLib.h>
+
 // Biblioteca para obtener credenciales
 #include "arduino_secrets.h"
 
 #define LAT "-34.62528799687441"
 #define LON "-58.464051513325735"
+
+#define INTERLINE 10
 
 // Configuración de pantalla
 // Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); 
@@ -56,9 +62,15 @@ void setup() {
     // Inicialización del display en vez de las líneas anteriores
     VextON();
     delay(100);
+    display.setContrast(255);
     display.init();
+    display.clear();
+    display.display();
+    display.drawString(0,0,"Hola mundirigillo");
+    display.display();
+    delay(1000);
 
-    display.setFont(ArialMT_Plain_10);
+    // display.setFont(ArialMT_Plain_10);
 }
 
 void loop() {
@@ -88,23 +100,39 @@ void loop() {
         
         // display.clearDisplay();
         display.clear();
-        display.setTextAlignment(TEXT_ALIGN_RIGHT);
+        // display.setTextAlignment(TEXT_ALIGN_RIGHT);
+
         // display.setTextSize(1);
-        display.setFont(ArialMT_Plain_10);
+
+        // display.setFont(ArialMT_Plain_10);
+
         // display.setTextColor(SSD1306_WHITE);
-        display.setColor(WHITE);
+        
+        // display.setColor(WHITE);
+        
         // display.setCursor(0, 0);
         // En principio no defino donde comienzo a escribir, veré en la próxima vuelta si puedo definirlo
-        display.print("\nCity: ");
-        display.println(weatherData["sys"]["name"]);
-        display.print("\nTemperature: ");
-        display.println(weatherData["main"]["temp"]);
-        display.print("\nPressure: ");
-        display.println(weatherData["main"]["pressure"]);
-        display.print("\nHumidity: ");
-        display.println(weatherData["main"]["humidity"]);
-        display.print("\nWind Speed: ");
-        display.println(weatherData["wind"]["speed"]);
+        // display.print("\nCity: ");
+        // display.println(weatherData["sys"]["name"]);
+        display.drawString(0,0,"City: " + String(weatherData["name"]));
+        long temp = (long) weatherData["main"]["temp"];
+        display.drawString(0,INTERLINE*1,"Temperature: " + String(temp));
+        int pres = (int) weatherData["main"]["pressure"];
+        display.drawString(0,INTERLINE*2,"Pressure: " + String(pres));
+        int hum = (int) weatherData["main"]["humidity"];
+        display.drawString(0,INTERLINE*3,"Humidity: " + String(hum));
+        long wind_s = (long) weatherData["wind"]["speed"];
+        display.drawString(0,INTERLINE*4,"Wind Speed: " + String(wind_s));
+        int time_var = (int) weatherData["dt"];
+        display.drawString(0,INTERLINE*5,"time acq: " + formatUnixToDMYHM(time_var));
+        // display.print("\nTemperature: ");
+        // display.println(weatherData["main"]["temp"]);
+        // display.print("\nPressure: ");
+        // display.println(weatherData["main"]["pressure"]);
+        // display.print("\nHumidity: ");
+        // display.println(weatherData["main"]["humidity"]);
+        // display.print("\nWind Speed: ");
+        // display.println(weatherData["wind"]["speed"]);
         display.display();
     } else {
         Serial.println("WiFi Disconnected");
@@ -134,6 +162,20 @@ String httpGETRequest(const char* serverName) {
     
     return payload;
 }
+
+
+String formatUnixToDMYHM(uint32_t unixSeconds) {
+  tmElements_t tm;
+  breakTime((time_t)unixSeconds, tm);
+
+  char buf[20];
+  snprintf(buf, sizeof(buf), "%02d/%02d/%04d %02d:%02d",
+           tm.Day, tm.Month, tm.Year + 1970,
+           tm.Hour, tm.Minute);
+
+  return String(buf);
+}
+
 
 void VextON(void)
 {
